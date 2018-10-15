290e53c9359e7d90c2ca483b03bf4431b2ca964e
Yoshito Tabuchi <ytabuchi.xlsoft@gmail.com>
2018-10-15 23:34:25 +0900

    fix filename

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -8,7 +8,7 @@ ls ../KudanAR/
 curl -OL https://github.com/ytabuchi/KudanAR-assets/raw/master/assets/cloud.mp4
 mv -f ./cloud.mp4 ./src/main/assets/cloud.mp4
 curl -OL https://github.com/ytabuchi/KudanAR-assets/raw/master/assets/waves.mp4
-mv -f ./cloud.mp4 ./src/main/assets/waves.mp4
+mv -f ./waves.mp4 ./src/main/assets/waves.mp4
 curl -OL https://github.com/ytabuchi/KudanAR-assets/raw/master/assets/kaboom.mp4
-mv -f ./cloud.mp4 ./src/main/assets/kaboom.mp4
+mv -f ./kaboom.mp4 ./src/main/assets/kaboom.mp4
 ls ./src/main/assets/
